c0656bbfddeaa5099a1168d610e8a3cd80b895d4
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-07-10 19:36:33 -0300

    Edit Dettail View Activity

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -7,7 +7,7 @@ import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, addressField;
+    private EditText nameField, addressField, numberField, businessField, provinceField;
     Contact receivedPersonInfo;
 
     @Override
@@ -16,12 +16,18 @@ public class DetailViewActivity extends Activity {
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
-        nameField = (EditText) findViewById(R.id.name);
-        addressField = (EditText) findViewById(R.id.business);
+        nameField     = (EditText) findViewById(R.id.name);
+        addressField  = (EditText) findViewById(R.id.business);
+        numberField   = (EditText) findViewById(R.id.number);
+        businessField = (EditText) findViewById(R.id.business);
+        provinceField = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
             addressField.setText(receivedPersonInfo.address);
+            numberField.setText(receivedPersonInfo.number);
+            businessField.setText(receivedPersonInfo.business);
+            provinceField.setText(receivedPersonInfo.province);
         }
     }
 
