0dc0c07079ce7ac4c5cd9abb8dc1e9c7b7492175
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-07-10 21:23:29 -0300

    Adding Javadoc

--- app/src/main/java/com/acme/a3csci3130/Contact.java
+++ app/src/main/java/com/acme/a3csci3130/Contact.java
@@ -10,6 +10,7 @@ import java.util.Map;
 /**
  * Class that defines how the data will be stored in the
  * Firebase databse. This is converted to a JSON format
+ * @author Mihyar Al Masalma
  */
 
 public class Contact implements Serializable {
@@ -24,7 +25,15 @@ public class Contact implements Serializable {
     public Contact() {
         // Default constructor required for calls to DataSnapshot.getValue
     }
-
+/**
+* Constructor for Contact Class
+ * @param uid Database User Unique Identifier
+ * @param name Name of the Business
+ * @param number Business Number
+ * @param business The name of the Business
+ * @param address  The Address of the Business
+ * @param province The Province where the Business is located
+* */
     public Contact(String uid, String name, String number, String business, String address, String province){
         this.uid = uid;
         this.name = name;
@@ -33,7 +42,10 @@ public class Contact implements Serializable {
         this.address = address;
         this.province = province;
     }
-
+/*
+* Business Data object to be stored
+ * @return result
+* */
     @Exclude
     public Map<String, Object> toMap(){
         HashMap<String, Object> result = new HashMap<>();
