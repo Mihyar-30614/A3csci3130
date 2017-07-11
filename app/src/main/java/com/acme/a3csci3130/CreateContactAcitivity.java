52bb25701253edb9deeb61ab6e50d26edf60a632
Mihyar Almasalma <eng.mihyear@gmail.com>
2017-07-10 21:32:56 -0300

    ADD Javadoc

--- app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateContactAcitivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+/*
+* Create contact user interface class
+* @author Mihyar Al Masalma
+* */
 public class CreateContactAcitivity extends Activity {
 
     private Button submitButton;
     private EditText nameField, numberField, addressField, businessField, provinceField;
     private MyApplicationData appState;
-
+/*
+* Create User Interface for the CreateContactActivity Class
+* @param savedInstanceState
+* */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +32,10 @@ public class CreateContactAcitivity extends Activity {
         businessField = (EditText) findViewById(R.id.business);
         provinceField = (EditText) findViewById(R.id.province);
     }
-
+/*
+* Push Contact info to database
+* @param v the View where the info comes from
+* */
     public void submitInfoButton(View v) {
         //each entry needs a unique ID
         String personID = appState.firebaseReference.push().getKey();
